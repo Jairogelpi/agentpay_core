283f644d64da3d02d754d231b91c59d94e2b6846
jairogelpi <gelpierreape@gmail.com>
2025-12-25 22:46:20 +0100

    Forzar actualizacion SaaS

--- main.py
+++ main.py
@@ -117,3 +117,4 @@ def approve_endpoint(token: str):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+# Ultima actualizacion: SaaS Edition
